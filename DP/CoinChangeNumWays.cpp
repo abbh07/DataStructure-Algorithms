9e18ab75bdcc8ca9410a023d892944bc12915937
Aakash Bhattacharya <aakashbhattacharya07@gmail.com>
2017-06-06 19:49:09 +0530

    DP

---
+++ DP/CoinChangeNumWays.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int coin_change(int *coins, int total, int size)
+{
+	vector< vector<int> > t(size+1,vector<int> (total+1));
+
+	for(int i=0;i<=size;i++)
+	{
+		t[i][0] = 1;
+	}
+
+	for(int i=1;i<=size;i++)
+	{
+		for(int j=1;j<=total;j++)
+		{
+			if(j<coins[i-1])
+			{
+				t[i][j] = t[i-1][j];
+			}
+			else
+			{
+				t[i][j] = t[i-1][j] + t[i][j-coins[i-1]];
+			}
+		}
+	}
+
+	return t[size][total];
+}
+
+int main()
+{
+	int coins[] = {1,2,3};
+	int size = 3;
+	int total = 5;
+	cout<<coin_change(coins,total,size)<<endl;
+	return 0;
+}
---
+++ DP/EditDistance.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int edit_distance(string s1, string s2)
+{
+	int l1 = s1.size();
+	int l2 = s2.size();
+
+	vector< vector<int> > t(l1+1, vector<int> (l2+1));
+	
+	for(int i=0;i<=l1;i++)
+		t[0][i] = i;
+	for(int i=0;i<=l2;i++)
+		t[i][0] = i;
+
+	for(int i=1;i<=l1;i++)
+	{
+		for(int j=1;j<=l2;j++)
+		{
+			if(s1[i] == s2[j])
+			{
+				t[i][j] = t[i-1][j-1];
+			}
+			else
+			{
+				t[i][j] = min(t[i][j-1], t[i-1][j]);
+				t[i][j] = min(t[i][j], t[i-1][j-1]) + 1;
+			}
+		}
+	}
+
+	return t[l1][l2];
+}
+
+int main()
+{
+	string s1 = "abcdef";
+	string s2 = "azced";
+	cout<<edit_distance(s1,s2)<<endl;
+	return 0;
+}
